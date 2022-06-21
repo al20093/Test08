947b1c2e1918d96cc4893e8219af4b67f5dde70e
Sano-Wataru <106137021+Sano-Wataru@users.noreply.github.com>
2022-06-21 20:52:33 +0900

    update SceneContents.java

--- SceneContents.java
+++ SceneContents.java
@@ -9,7 +9,13 @@ package application;
 import java.util.HashMap;
 import java.util.Map;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 
 public class SceneContents
 {
@@ -39,5 +45,29 @@ public class SceneContents
 		sceneMap.put("home", home.getScene());
 		sceneMap.put("feedback", feedback.getScene());
 		sceneMap.put("area", area.getScene());
+	}
+	
+	static StackPane subTitle(String title)
+	{
+		StackPane sp = new StackPane();
+		Rectangle rect = new Rectangle(50, 50, 150, 30);
+		Label lb = new Label(title);
+		
+		lb.setFont(Font.font(Constant.FONTFAMILY,Constant.FONTWEIGHT,20));
+		
+		rect.setFill(Color.LIGHTSKYBLUE);
+		rect.setStroke(Color.BLACK);
+		rect.setArcWidth(10);
+		rect.setArcHeight(10);
+		
+		sp.getChildren().addAll(rect, lb);
+		sp.setAlignment(Pos.CENTER);
+		sp.setTranslateX(-120);
+		return sp;
+	}
+	
+	static void Lagrange(double x, double...pos)
+	{
+		
 	}
 }
