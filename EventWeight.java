be4895772da1b6b783ab3f0e898a8326cb8729ad
al20037 <al20037@shibaura-it.ac.jp>
2022-06-25 21:21:44 +0900

    0625

--- EventWeight.java
+++ EventWeight.java
@@ -1,6 +1,16 @@
+/**********************************************/
+/*author:金　東柱 6/25更新
+/*C4:重み処理部所属
+/*EventWeight:
+/*重み設定画面のイベント処理を記述したクラス
+/**********************************************/
+
 package application;
 
+import java.util.ArrayList;
+
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 //イベント処理クラス
 public class EventWeight
@@ -18,5 +28,22 @@ public class EventWeight
 		{ weight.assignSceneToStage("home"); });
 	}
 	
+	void weightSave(Button bt, TextField chtf1, TextField chtf2, 
+							TextField chtf3, TextField chtf4, TextField chtf5)
+	{
+		bt.setOnAction((ActionEvent) ->
+		{ 	
+			ArrayList<Double> wdata = new ArrayList<>();
+			wdata.add(Double.parseDouble(chtf1.getText()));
+			wdata.add(Double.parseDouble(chtf2.getText()));
+			wdata.add(Double.parseDouble(chtf3.getText()));
+			wdata.add(Double.parseDouble(chtf4.getText()));
+			wdata.add(Double.parseDouble(chtf5.getText()));
+			
+			System.out.println(wdata);
+			UserData uData = new UserData();
+			uData.weightWrite(wdata);
+		});
+	}
 
 }
