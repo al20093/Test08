ee16a0157aadfda336bb9801233dbfa82940df4f
AishaNakui <106137043+AishaNakui@users.noreply.github.com>
2022-06-28 17:18:14 +0900

    Update EventArea.java

--- EventArea.java
+++ EventArea.java
@@ -1,6 +1,5 @@
-
 /****************************************/ 
-/*author:名久井愛紗 6/19更新 
+/*author:名久井愛紗 6/28更新 
 /*C5:服装提案部所属 
 /*EventArea: 
 /*服装提案部でのイベント処理を記述する
@@ -8,7 +7,11 @@
 
 package application;
 
+import java.util.List;
+
+import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 
 public class EventArea {
 SceneArea area;
@@ -18,10 +21,17 @@ SceneArea area;
 		this.area = area;
 	}
 	
-	void transitionClothingresult(Button bt)
+	void transitionResult(Button bt, ListView<String> lv, ObservableList<String> items)
 	{
 		bt.setOnAction((ActionEvent) ->
-			{ area.assignSceneToStage("clothing"); });
+			{ 
+				area.assignSceneToStage("clothing"); 
+				String areaname;
+				//areaname = lv.getItems(items);
+				areaname = lv.getSelectionModel().getSelectedItem();
+				List<Clothes> result = new DataClothing().getClothes(areaname);
+				new DataClothing().orderList(result);
+			});
 	}
 	
 	void transitionHome(Button bt)
@@ -29,4 +39,5 @@ SceneArea area;
 		bt.setOnAction((ActionEvent) ->
 			{ area.assignSceneToStage("home"); });
 	}
+	
 }
